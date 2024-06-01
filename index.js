8e209951da8ca51d95d644c41eb3f836199174fc
unknown <21b41.raksha@sjec.ac.in>
2024-06-01 19:06:02 +0530

    index.js by rk

---
+++ index.js
@@ -0,0 +1,13 @@
+const wrapper=document.querySelector('.wrapper');
+const loginLink=document.querySelector('.login-link');
+
+loginLink.addEventListener('click',()=> {
+    wrapper.classList.remove('active');
+});
+
+
+// 24.11 sec in vid
+
+
+
+
